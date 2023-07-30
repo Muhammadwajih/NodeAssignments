e301983c140f10b573fe41c3bc5a0d74b02c8d88
Muhammad Wajih Ul Murtaza Sheikh <muhammad.wajih@systemsltd.com>
2023-07-30 13:22:38 +0500

    password field added

--- models/student.js
+++ models/student.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const StudentSchema = new mongoose.Schema({
     StudentId: {type : Number, required : true},
     Name: {type : String},
+    Password : {type : String , required: true},
     Roll: {type : Number},
     Birthday: {type : String},
     Address: { type : String}
